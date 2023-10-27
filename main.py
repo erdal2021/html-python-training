ae47e70a1c852b84b3dd8bfbf59ca353f1eb9efa
erdal2021 <87144653+erdal2021@users.noreply.github.com>
2023-10-27 18:17:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-#from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 name = "Erdal Erdogan"
 
-skills =["Hard- und Software", "Betriebssysteme", "Netzwerke", "Open Source Software", "Cloud Computing", "AWS", "Git", "Python, VSCode, html, css, js, php", "CMS", "Musik"]
+skills =["Hard- und Software","Betriebssysteme", "Netzwerke", "Open Source Software", "Cloud Computing", "AWS", "Git", "Python, VSCode, css, html, js, php", "CMS", "Musik"]
 
 
 @app.get("/",response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request" : request, "name" : name, "skills" : skills})
-# @app.get("/kontakt", response_class=HTMLResponse)
-# async def get_form(request: Request):
-#         return templates.TemplateResponse("kontakt.html", {"request" : request})
-# @app.post("/kontakt", response_class=HTMLResponse)
-# async def post_form(request: Request):
-#         return templates.TemplateResponse("kontakt.html", {"request" : request})
+
+@app.get("/kontakt", response_class=HTMLResponse)
+async def get_form(request: Request):
+    return templates.TemplateResponse("contact.html", {"request" : request})
+
+@app.post("/submit/")
+async def submit_form(request: Request, name: str = Form(...), email: str = Form(...), message: str = Form(...)):
+    return templates.TemplateResponse("confirmation.html", {"request": request, "name": name})
 
 if __name__ == "__main__":
        import uvicorn 
        uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-
-
-
-
-
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# templates = Jinja2Templates(directory="templates")
-
-
-# @app.get("/items/{id}", response_class=HTMLResponse)
-# async def read_item(request: Request, id: str):
-#     return templates.TemplateResponse("item.html", {"request": request, "id": id})
